28387fad733cf59cdd7cd863669b6afa17731b2e
elizabethgim <yujingim@naver.com>
2023-11-17 16:29:23 +0900

    feat: nutrient repository 추가

--- src/modules/food.module.ts
+++ src/modules/food.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FoodService } from '../service/food.service';
 import { FoodController } from '../controller/food.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Nutrient } from 'src/entities/nutrient.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Nutrient])],
   controllers: [FoodController],
   providers: [FoodService],
 })
