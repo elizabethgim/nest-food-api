58c180a5293560b67e4fd561bb927060833e606c
elizabethgim <yujingim@naver.com>
2023-11-17 17:04:26 +0900

    feat: csv 파일 디렉터리 지정하기

--- src/db/excel-to-csv.ts
+++ src/db/excel-to-csv.ts
@@ -4,7 +4,7 @@ import * as XLSX from 'xlsx';
 async function excelToCsv() {
     console.log(__dirname);
     const workbook = XLSX.readFile(__dirname + "/processed_data.xlsx");
-    XLSX.writeFile(workbook, "changed_Data.csv", { bookType: "csv" });
+    XLSX.writeFile(workbook, "src/db/changed_Data.csv", { bookType: "csv" });
 }
 
 excelToCsv();
