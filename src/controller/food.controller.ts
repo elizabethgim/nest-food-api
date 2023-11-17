b4b6614d968a8f474b5088e332b304ea369788ac
elizabethgim <yujingim@naver.com>
2023-11-17 14:40:19 +0900

    feat: query parameter 추가

--- src/controller/food.controller.ts
+++ src/controller/food.controller.ts
@@ -1,9 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { FoodService } from '../service/food.service';
+import { Food } from 'src/dto/food.dto';
 
 @Controller('food')
 export class FoodController {
   constructor(private readonly foodService: FoodService) { }
+
+  @Get()
+  async searchFood(
+    @Query('name') name?: string,
+    @Query('year') year?: string,
+    @Query('manufacturer') manufacturer?: string,
+    @Query('foodCode') foodCode?: string,
+  ) {
+    const query: Food = {
+      name: name,
+      year: +year,
+      maker: manufacturer,
+      code: foodCode
+    }
+    return await this.foodService.find(query);
+  }
 
   @Get()
   findAll() {
