7474510c881ebc54e41b5f1a8789f988b8999f7b
elizabethgim <yujingim@naver.com>
2023-11-17 16:51:25 +0900

    feat: validator 추가

--- src/dto/food.dto.ts
+++ src/dto/food.dto.ts
@@ -1,6 +1,15 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class Food {
+    @IsString()
     name: string
+
+    @IsNumber()
     year: number
+
+    @IsString()
     maker: string
+
+    @IsString()
     code: string
 }
