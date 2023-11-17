a5cb5a3109794498b7c0d55946839227f6888cb1
elizabethgim <yujingim@naver.com>
2023-11-17 13:24:29 +0900

    create: nutrient entity 출력 인자

---
+++ src/entities/nutrient.entity.ts
@@ -0,0 +1,55 @@
+import { Column, Entity, PrimaryColumn } from "typeorm"
+
+@Entity({ name: 'foods' })
+export class Nutrient {
+    @PrimaryColumn()
+    id: number
+
+    @Column({ type: "varchar" })
+    code: string
+
+    @Column({ type: "varchar" })
+    group_name: string
+
+    @Column({ type: "varchar" })
+    name: string
+
+    @Column({ type: "int" })
+    year: number
+
+    @Column({ type: "varchar" })
+    maker_name: string
+
+    @Column({ type: "varchar" })
+    ref_name: string
+
+    @Column({ type: "int" })
+    serving_size: number
+
+    @Column({ type: "double" })
+    calorie: number
+
+    @Column({ type: "double" })
+    carbohydrate: number
+
+    @Column({ type: "double" })
+    protein: number
+
+    @Column({ type: "double" })
+    province: number
+
+    @Column({ type: "double" })
+    sugars: number
+
+    @Column({ type: "double" })
+    salt: number
+
+    @Column({ type: "double" })
+    cholesterol: number
+
+    @Column({ type: "double" })
+    saturated_fatty_acids: number
+
+    @Column({ type: "double" })
+    trans_fat: number
+}
