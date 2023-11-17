104d6e8d765b2fc7ee21e4785fee20f87ce23935
elizabethgim <yujingim@naver.com>
2023-11-17 13:59:36 +0900

    chore: 환경 변수 설정

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 import { FoodModule } from './food.module';
 
 @Module({
-  imports: [FoodModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      entities: [
+        "src/entities/*.ts"
+      ],
+      "logging": true
+    }),
+    FoodModule
+  ],
 })
 export class AppModule { }
